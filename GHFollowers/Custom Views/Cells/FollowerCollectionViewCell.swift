6c1ac116db242df24cad971521afa6cd7079087c
Caio Luna <ccluna@gmail.com>
2026-01-10 14:05:45 -0300

    Configure FollowerCollectionViewCell

---
+++ GHFollowers/Custom Views/Cells/FollowerCollectionViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  FollowerCollectionViewCell.swift
+//  GHFollowers
+//
+//  Created by Caio Luna on 10/01/26.
+//
+
+import UIKit
+
+class FollowerCollectionViewCell: UICollectionViewCell {
+	
+	static let reuseId = "FollowerCell"
+	
+	let avatarImageView = GFAvatarImageView(frame: .zero)
+	let usernameLabel 	= GFTitleLabel(textAlignment: .center, fontSize: 16)
+	
+	let padding: CGFloat = 8
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		configureAvatarImageView()
+		configureUsernameLabel()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	
+	func set(follower: Follower) {
+		usernameLabel.text = follower.login
+	}
+	
+	
+	private func configureAvatarImageView() {
+		addSubview(avatarImageView)
+		
+		NSLayoutConstraint.activate([
+			avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+			avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+			avatarImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+			avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+		])
+	}
+	
+	
+	private func configureUsernameLabel() {
+		addSubview(usernameLabel)
+	
+		NSLayoutConstraint.activate([
+			usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+			usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+			usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+			usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+		])
+	}
+}
---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Caio Luna on 10/01/26.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+	
+	let placeholderImage = UIImage(named: "avatar-placeholder")
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		configure()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	
+	private func configure() {
+		translatesAutoresizingMaskIntoConstraints = false
+		
+		layer.cornerRadius 	= 10
+		clipsToBounds 			= true
+		image 							= placeholderImage
+	}
+}
